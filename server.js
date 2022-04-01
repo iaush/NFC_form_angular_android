ee239f74727775a661a62b8a9a30e9d93e2dca2c
iaush <ryanchanenator@gmail.com>
2022-04-01 11:32:18 +0800

    heroku express http

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
+var proxy = require('express-http-proxy')
+
+
 const app = express();
 app.use(express.static("./dist/nfc-form"));
 
 app.get("/*", function(req,res){
     res.sendFile("index.html", {root:"dist/project-name"});
 })
+
+app.use('https://clf-form.herokuapp.com/forms', proxy('https://docs.google.com/forms'))
 
 app.listen(process.env.PORT || 8080);
 
