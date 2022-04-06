0d24547b9eb59d2e8fc0820d60390503a5221bfe
iaush <ryanchanenator@gmail.com>
2022-04-06 11:12:58 +0800

    compress

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const proxy = require('express-http-proxy')
 const url = require('url')
-
+const compression = require('compression')
 
 const app = express();
 app.use(express.static("./dist/nfc-form"));
-
+app.use(compression())
 
 app.get("/*", function(req,res){
     res.sendFile("index.html", {root:"dist/project-name"});
